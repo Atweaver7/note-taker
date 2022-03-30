90c3ab3eb09b1cb216156259a4720e9b0888a311
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-03-29 18:38:59 -0700

    heroku round 8

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./Routes/apiRoutes");
+const htmlRoutes = require("./Routes/htmlRoutes");
 // more tests
 
 
